2740eba2e449816d894de1ffe30a27acc59bf644
tfuquirk <timothyfquirk@gmail.com>
2018-12-13 17:08:20 -0500

    2 files updated to pass tests

---
+++ lib/project.rb
@@ -0,0 +1,24 @@
+require "pry"
+
+class Project
+
+  attr_accessor :backers, :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+
+    backer.backed_projects << self
+  
+
+    # length = Backer.backed_projects.length
+    # break if length = length + 1
+
+
+  end
+
+end
